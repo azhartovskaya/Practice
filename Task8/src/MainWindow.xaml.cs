d02671b73b97ee986fc9bc0a9f3c47ac79066d25
azhartovskaya <azhartovskaya@yandex.ru>
2025-03-10 10:13:16 +0700

    Add files via upload

---
+++ Task8/src/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Task8
+{
+    public partial class MainWindow : Window
+    {
+        private bool isDragging = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            textBox.Text += button.Content.ToString() + " ";
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "Добавлено. ";
+        }
+
+        private void FontSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            textBox.FontSize = e.NewValue;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string colorName = selectedItem.Content.ToString();
+                switch (colorName)
+                {
+                    case "Красный":
+                        this.Background = Brushes.Red;
+                        break;
+                    case "Зеленый":
+                        this.Background = Brushes.Green;
+                        break;
+                    case "Синий":
+                        this.Background = Brushes.Blue;
+                        break;
+                }
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.SelectedItem is ListBoxItem selectedItem)
+            {
+                textBox.Text = selectedItem.Content.ToString();
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            progressBar.Visibility = Visibility.Visible;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            progressBar.Visibility = Visibility.Hidden;
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (datePicker.SelectedDate.HasValue)
+                textBox.Text = datePicker.SelectedDate.Value.ToShortDateString();
+        }
+
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            ellipse.CaptureMouse();
+        }
+
+        private void Ellipse_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point position = e.GetPosition(this.canvas);
+                Canvas.SetLeft(ellipse, position.X - ellipse.Width / 2);
+                Canvas.SetTop(ellipse, position.Y - ellipse.Height / 2);
+            }
+        }
+
+        private void Ellipse_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            ellipse.ReleaseMouseCapture();
+        }
+    }
+}
